18f7959d43a6a276ff3129350c8e9d2845d8a75d
lishuang <lish516@126.com>
2017-07-17 18:07:57 +0800

    Finish the basic regex thigns.

--- main.go
+++ main.go
@@ -4,5 +4,5 @@ import "stone/model"
 
 func main() {
 	lexer := model.Lexer{}
-	lexer.ReadLine("inta=100;")
+	lexer.ReadLine("x = m ? 12: 23; int c = a + 200; int a = 00100; x==y; string b = \"hello \\ \n \"  world\"; //I'm comment")
 }
--- model/lexer.go
+++ model/lexer.go
@@ -10,12 +10,28 @@ type Lexer struct {
 
 func (l *Lexer) ReadLine(line string) {
 
-	reg, _ := regexp.Compile(`([0-9]+)|([a-z]+)`)
+	//空格 \s*
+	// 注释 //
+	// 字符串 "(\\"|\\\\|\\n|[^"])*" 里面可以是 \" 或 \\ 或 \n 或 非"的任何字符
+	// 标识符 [A-Z_a-z][A-Z_a-z0-9]*|==|<=|>=|&&|\|\| 不以数字开头 或 == 或 <= 或 >= 或 && 或 || 或
+	// !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ => !"#\$%&'\(\)\*\+,-\./:;<=>\?@\[\\\]\^_`\{\|\}~
+	reg, _ := regexp.Compile(`\s*(//.*)|([0-9]+)|([a-z]+)|("(\\"|\\\\|\\n|[^"])*")|([A-Z_a-z][A-Z_a-z0-9]*|==|<=|>=|&&|\|\||[\=\+\-\*\/\(\)\{\}\[\]\|\&\~\?\:\.\%\#\@\;])`)
 
-	all := reg.FindAllString(line, 1)
+	all := reg.FindAllString(line, -1)
 	fmt.Printf("%q\n", all)
 
-	allIndex := reg.FindAllStringIndex(line, -1);
-	fmt.Printf("%q\n", allIndex)
+	//allIndex := reg.FindAllStringIndex(line, -1);
+	//for _, v := range allIndex {
+	//	fmt.Println(v)
+	//}
+	//
+	subMatch := reg.FindAllSubmatch([]byte(line), -1)
+	fmt.Println("FindSubmatch", subMatch)
+	for i, v := range subMatch {
+		fmt.Printf("i = %d \n", i)
+		for _, v1 := range v {
+			fmt.Println(string(v1))
+		}
+	}
 
 }
