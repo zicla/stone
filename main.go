ab27cf2850256fe9e5f9ab6a3bff66babf6e2743
Zic <logzcx@126.com>
2017-07-24 19:52:43 +0800

    Remove a comma.

--- main.go
+++ main.go
@@ -9,7 +9,8 @@ func main() {
 
 	// x   = m ? 12: 23; int c = a + 200;  int a = 00100; x==y; string b = \"hello \\ \n \"  world\"; //I'm comment
 
-	//codes := input.GetConsoleInputText();
+	//codes := input.GetConsoleInputText()
 	codes := "m ? 12: 23; int c = a + 200;  int a = 00100; x==y; string b = \"hello \\ \n \"  world 世界你好啊\"; //I'm comment";
 	lexer.ReadLine(codes)
+
 }
